e8b3f1a6931d94350a8efe1cd9dbffc3bcfbe1b1
kermattC <chan.matt624@gmail.com>
2024-11-26 12:56:53 -0500

    Created spring proj

---
+++ src/main/java/com/mlbstats/baseball_stats/BaseballStatsApplication.java
@@ -0,0 +1,13 @@
+package com.mlbstats.baseball_stats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseballStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseballStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mlbstats/baseball_stats/BaseballStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlbstats.baseball_stats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseballStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
